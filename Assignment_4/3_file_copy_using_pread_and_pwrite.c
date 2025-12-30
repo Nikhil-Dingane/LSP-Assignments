ecd8ac01a68b2eea5548706de1263783114c0ab1
nikhil <nikhil.nikhil.dingane1@gmail.com>
2025-12-30 18:50:37 +0530

    Assignment 4.3

---
+++ Assignment_4/3_file_copy_using_pread_and_pwrite.c
@@ -0,0 +1,146 @@
+/********************************************************************
+
+  3. File Copy Using pread & pwrite
+
+    Problem Statement:
+	Copy a file using pread() and pwrite() without changing the file offset.
+
+    Input:
+	Source and destination file names.
+
+    Output:
+	Copied file identical to source.
+
+********************************************************************/
+
+#include<stdio.h>
+#include<unistd.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<string.h>
+
+#define BLOCK_SIZE 1024
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL copyData(int srcFileFd, int dstFileFd)
+{
+	char buffer[BLOCK_SIZE] = {'\0'};
+	int iRet = 0, srcFileReadOffset = 0, dstFileWriteOffset = 0, writeRet = 0;
+
+	errno = 0;
+	while((iRet = pread(srcFileFd, buffer, BLOCK_SIZE, srcFileReadOffset)) != 0)
+	{
+		if(errno)
+		{
+			perror("Unable to read from the source file");
+			return FALSE;
+		}
+
+		srcFileReadOffset += iRet;
+
+		writeRet = pwrite(dstFileFd, buffer, iRet, dstFileWriteOffset);
+
+		if(errno)
+		{
+			perror("Unable to write to the destination file");
+			return FALSE;
+		}
+
+		dstFileWriteOffset += writeRet;
+		memset(buffer, '\0', BLOCK_SIZE);
+	}
+
+	return TRUE;
+}
+
+BOOL enableFileWritePermissions(char* dstFileName)
+{
+	struct stat fileStat;
+
+	if(stat(dstFileName, &fileStat) == -1)
+	{
+		perror(dstFileName);
+		return FALSE;
+	}
+
+	if(chmod(dstFileName, fileStat.st_mode | S_IWUSR | S_IWGRP | S_IWOTH) == -1)
+	{
+		perror("Unable to change the mode of destination file");
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+BOOL openDestinationFileToWrite(int* dstFileFd, char* dstFileName)
+{
+	*dstFileFd = open(dstFileName, O_WRONLY | O_TRUNC);
+	if(*dstFileFd == -1)
+	{
+		if((errno == ENOENT) )
+		{
+			errno = 0;
+			if((*dstFileFd = creat(dstFileName, 0777)) == -1)
+			{
+				perror(dstFileName);
+				return FALSE;
+			}
+		}
+		else if(errno == EACCES )
+		{
+			errno = 0;
+			if(!enableFileWritePermissions(dstFileName))
+				return FALSE;
+
+			errno = 0;
+			if((*dstFileFd = open(dstFileName, O_WRONLY)) == -1)
+			{
+				perror(dstFileName);
+				return FALSE;
+			}
+		}
+		else
+		{
+			perror(dstFileName);
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 3)
+	{
+		errno = EINVAL;
+		perror(argv[0]);
+		printf("Usage: %s <source file path> <destination file path>\n", argv[0]);
+		return -1;
+	}
+
+	int srcFileFd = open(argv[1], O_RDONLY);
+
+	if(errno == -1)
+	{
+		perror(argv[1]);
+		return -2;
+	}
+
+	int dstFileFd = -1;
+
+	if(!openDestinationFileToWrite(&dstFileFd, argv[2]))
+		return -3;
+
+	if(!copyData(srcFileFd, dstFileFd))
+		return -4;
+
+	printf("Data from %s file is copied to %s file successfully\n", argv[1], argv[2]);
+
+	return 0;
+}
