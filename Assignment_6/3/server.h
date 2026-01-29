a85e325f3cf6b6eba7644685c8c99ef5140a3b86
nikhil <nikhil.nikhil.dingane1@gmail.com>
2026-01-29 21:18:14 +0530

    Skipped changes

--- Assignment_6/3/server.h
+++ Assignment_6/3/server.h
@@ -3,5 +3,7 @@
 
 double Addition(double a, double b);
 double Substraction(double a, double b);
+double Multiplication(double a, double b);
+double Division(double a, double b);
 
 #endif // SERVER_H
